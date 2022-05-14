ed0c597ce4f232bc19ee6e7c5ea03dc72697b33f
Jungwone <prsjb77@gmail.com>
2022-05-14 23:02:04 +0900

    암송 서비스 클래스 추가

---
+++ src/service/PassageService.ts
@@ -0,0 +1,31 @@
+import moment, { Moment } from "moment";
+import { Passage } from "../data";
+
+export default class PassageService {
+  constructor(private passages: Passage[]) {}
+
+  /**
+   * 이번주 기간에 해당하는 암송 구절 찾기
+   */
+  private findPassagesForThisWeek(today: Moment, passage: Passage) {
+    const startDate = moment(passage.startDate);
+    const endDate = moment(passage.endDate);
+
+    if (today.isSameOrBefore(endDate) && today.isSameOrAfter(startDate)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  /**
+   * 이번주 암송 데이터 얻어오기
+   */
+  getPassagesForThisWeek() {
+    const today = moment();
+
+    return this.passages.filter((passage) =>
+      this.findPassagesForThisWeek(today, passage)
+    );
+  }
+}
