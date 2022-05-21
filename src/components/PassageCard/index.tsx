5f0021ac893e7b320b0c789e543e16609bcc3a8b
Jungwone <prsjb77@gmail.com>
2022-05-22 01:15:07 +0900

    암송카드 컴포넌트

--- src/components/PassageCard/index.tsx
+++ src/components/PassageCard/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Passage } from "../../data";
 import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
@@ -12,7 +11,7 @@ const PassageCard = ({ passage }: Props) => {
     <Wrapper>
       <Address>{passage.address}</Address>
       <Content>{passage.content}</Content>
-      <Link to={`/test/${passage.id}`}>테스트하기</Link>
+      <TestButton to={`/test/${passage.id}`}>테스트</TestButton>
     </Wrapper>
   );
 };
@@ -20,13 +19,36 @@ const PassageCard = ({ passage }: Props) => {
 export default PassageCard;
 
 const Wrapper = styled.div`
-  max-width: 380px;
   background-color: #fff;
-  margin: 1em;
+  margin-left: 12px;
+  margin-right: 12px;
+  margin-bottom: 1.5em;
+  padding: 1em;
+  line-height: 1.3;
+  border-radius: 24px;
+  box-shadow: 5px 6px 15px 0px rgba(0, 0, 0, 0.8);
 `;
 
 const Address = styled.div`
   font-weight: bold;
+  margin-bottom: 12px;
 `;
 
-const Content = styled.div``;
+const Content = styled.pre`
+  text-align: left;
+  white-space: pre-line;
+`;
+
+const TestButton = styled(Link)`
+  text-decoration: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 36px;
+  background-color: #f5566c;
+  color: #fff;
+  border-radius: 30px;
+  margin-left: auto;
+  margin-top: 12px;
+`;
